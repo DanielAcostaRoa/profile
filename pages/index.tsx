73f8fcf912e5f42a7a11dad7ff537a91a9505208
DanielAcostaRoa <danielroa96@outlook.com>
2022-07-12 05:40:21 -0500

    fix: change style on image background

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,23 +7,41 @@ const Home: NextPage = () => {
       <Head>
         <title>Daniel Acosta</title>
       </Head>
-      <main className="profile-page h-screen w-screen">
+      <main className="profile-page">
         <section className="relative block h-500-px">
           <div
-            className="absolute top-0 w-full h-full bg-cover bg-center"
+            className="absolute top-0 w-full h-full bg-center bg-cover"
             style={{
-              backgroundImage:
-                "url(https://images.unsplash.com/photo-1499336315816-097655dcfbda?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=2710&amp;q=80)",
+              backgroundImage: `url('/images/bgprofile.jpeg')`,
             }}
           >
             <span
               id="blackOverlay"
-              className="w-full h-full absolute opacity-60 bg-black"
+              className="w-full h-full absolute opacity-50 bg-black"
             ></span>
           </div>
+          <div
+          className="top-auto bottom-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden h-70-px"
+          style={{ transform: "translateZ(0px)" }}
+        >
+          <svg
+            className="absolute bottom-0 overflow-hidden"
+            xmlns="http://www.w3.org/2000/svg"
+            preserveAspectRatio="none"
+            version="1.1"
+            viewBox="0 0 2560 100"
+            x="0"
+            y="0"
+          >
+            <polygon
+              className="text-blueGray-200 fill-current"
+              points="2560 0 2560 100 0 100"
+            ></polygon>
+          </svg>
+        </div>
         </section>
-        <section className="relative block h-500-px">
-          <div className="container mx-auto px-4 py-1">
+        <section className="relative py-16 bg-blueGray-200">
+          <div className="container mx-auto px-4">
             <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-1 shadow-xl rounded-lg -mt-64">
               <div className="px-10">
                 <div className="flex flex-wrap justify-center">
